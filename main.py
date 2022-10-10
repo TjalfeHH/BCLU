a70944f3294514bb68ca1f1af46ffe7511616ada
Tjalfe Haraldsson <Tjalfe.H.H@gmail.com>
2022-10-10 23:24:26 +0200

    modified main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-console_input = ""
+variables = {'file': ""}
 
 def print_menu():
     print("middle grip - 0")
@@ -11,27 +11,56 @@ def print_menu():
     print("write \"use\" followed by an option number")
     print("")
 
-def print_help():
-    pass
 
-print_menu()
+def clean_input(_input):
 
-while True:
-    console_input = input("BCLU >").lower()
+    #Removes all spaces in the beginning of input
+    while(_input[0] == " "):
+        _input = _input[1:len(_input)]
 
-    if(console_input == "exit" or console_input == "quit"):
-        os._exit(0)
+    #Removes all spaces in the end of input
+    while(_input[len(_input)-1] == " "):
+            _input = _input[0:len(_input)-1]
 
-    if(console_input == "menu"):
-        print_menu()
+    _input_remove_list = []
 
-    if(console_input == "help"):
-        print_help()
+    #Detects all double spaces and adds their location to _input_remove_list
+    for i in range(1, len(_input)-1):
+        if(_input[i-1] == " " and _input[i] == " "):
+            _input_remove_list.append(i)
 
-    if(console_input.startswith("use")):
-        option = console_input[4:]
+    #Removes all the indexes saved in _input_remove_list
+    for i in range(len(_input_remove_list)):
+        _input = _input[:_input_remove_list[i]] + _input[_input_remove_list[i] + 1:]
 
-        if(option == "0"):
-            print("=000")
+        for i in range(len(_input_remove_list)):
+            _input_remove_list[i] -= 1
 
-    
+    equal_pos = _input.find("=")
+
+    #removes spaces beside equal sign
+    if(equal_pos != -1):
+        if(equal_pos-1 > 0):
+            if(_input[equal_pos-1] == " "):
+                _input = _input[:equal_pos-1] + _input[equal_pos-1 + 1:]
+                equal_pos = _input.find("=")
+
+        if(equal_pos+1 < len(_input)):
+            if(_input[equal_pos+1] == " "):
+                _input = _input[:equal_pos+1] + _input[equal_pos+1 + 1:]
+
+
+    return _input
+
+def main():
+    while True:
+        console_input = clean_input(input("BCLU>"))
+
+        elif(console_input.startswith("set"):
+             pass
+
+main()
+
+
+
+
